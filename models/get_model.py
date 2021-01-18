f94dd53e61f59ef21b6beba1bc32cda61a845f28
st164137@uni-stuttgart.de <st_st164137@uc2n997.localdomain>
2021-01-18 21:42:02 +0100

    include raft

--- models/get_model.py
+++ models/get_model.py
@@ -1,8 +1,11 @@
-from .pwclite import PWCLite
+#from .pwclite import PWCLite
+from .raft import RAFT
 
 def get_model(cfg):
     if cfg.type == 'pwclite':
         model = PWCLite(cfg)
+    elif cfg.type == 'raft':
+        model = RAFT(cfg)
     else:
         raise NotImplementedError(cfg.type)
     return model
